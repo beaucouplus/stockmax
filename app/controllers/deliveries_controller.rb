57f0525ffbabb9bc544767d6a9468bf50adbea86
beaucouplus <maxime@beaucouplus.com>
2020-03-27 15:04:45 +0100

    controllers > delivery actions work

--- app/controllers/deliveries_controller.rb
+++ app/controllers/deliveries_controller.rb
@@ -1,4 +1,18 @@
 class DeliveriesController < ApplicationController
   def create
+    @delivery = Delivery.new(delivery_params)
+    @delivery.save
+    render json: @delivery
+  end
+
+  def index
+    @deliveries = Delivery.all
+    render json: @deliveries
+  end
+
+  private
+
+  def delivery_params
+    params.require(:delivery).permit(:name)
   end
 end
