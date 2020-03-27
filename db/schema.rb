2bc23580a83c04488e56b4baabd9bd09445f5bfb
beaucouplus <maxime@beaucouplus.com>
2020-03-27 15:03:10 +0100

    models > added quantity ref to delivery and defaulted to 0

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_27_132533) do
+ActiveRecord::Schema.define(version: 2020_03_27_140122) do
 
   create_table "deliveries", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "quantity_ref", default: 0
   end
 
   create_table "delivery_items", force: :cascade do |t|
