e333711f5472f2cc81603c6beed563e078c37066
beaucouplus <maxime@beaucouplus.com>
2020-03-27 14:24:44 +0100

    models > added delivery with pname

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_27_132122) do
+ActiveRecord::Schema.define(version: 2020_03_27_132331) do
+
+  create_table "deliveries", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "items", force: :cascade do |t|
     t.string "name"
