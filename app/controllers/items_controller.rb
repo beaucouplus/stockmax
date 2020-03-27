024ff0917b9a3c548069adc33f3304ae7a5a1f27
beaucouplus <maxime@beaucouplus.com>
2020-03-27 14:58:56 +0100

    controllers > item actions work

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,18 @@
 class ItemsController < ApplicationController
   def create
+    @item = Item.new(item_params)
+    @item.save
+    render json: @item
+  end
+
+  def index
+    @items = Item.all
+    render json: @items
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :place)
   end
 end
